50553c722861155bd05e57a58968d9bbae2b8383
nikolaichukkk <59363088+nikolaichukkk@users.noreply.github.com>
2022-07-24 19:52:24 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper('.swiper-container', {
+    speed: 400,
+    spaceBetween: 100
+});
+
+var menuButton = document.querySelector('.menu-button');
+var menu = document.querySelector('.header')
+
+menuButton.addEventListener('click', function(){
+	menuButton.classList.toggle('menu-button-active');
+	menu.classList.toggle('header-active')
+})
